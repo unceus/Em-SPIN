4a169391197b56ec6d533009a4cf2b4b20878ba0
Michael R. Joseph <michael@unce.us>
2016-09-15 11:46:14 -0700

    Use ember-data and add some simple routing

--- app/routes/spin.js
+++ app/routes/spin.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return [
-      Em.Object.create({questionText: 'I am afraid of people in authority'}),
-      Em.Object.create({questionText: 'I am bothered by blushing in front of people'}),
-      Em.Object.create({questionText: 'Parties and social events scare me'})
-    ]
-  }
+  model() { return this.get('store').findAll('spin-question') }
 });
